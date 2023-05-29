3d60d7ed815940a9c51b827761ac5508e1e61b5c
Anas <anas9991az@gmail.com>
2023-05-29 12:03:10 +0530

    transformed response data

--- RecipeBook/src/app/services/data-storage.service.ts
+++ RecipeBook/src/app/services/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "./recipe.service";
 import { Recipe } from "../shared/recipe.model";
+import { map } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +25,13 @@ export class DataStorage{
 
   fetchRecipes(){
     this.http.get<Recipe[]>('https://ng-recipebook-2f304-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(map(
+      recipes=>{
+        return recipes.map(recipe=>{
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        });
+      }
+    ))
     .subscribe((recipes)=>{
       this.recipeService.setRecipes(recipes);
     })
