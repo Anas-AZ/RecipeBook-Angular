be10233e1b2e97d2308dd2bb0ce97fd86d1a0e5f
Anas <anas9991az@gmail.com>
2023-05-30 12:23:15 +0530

    Send Sign Up request

--- RecipeBook/src/app/auth/auth/auth.component.ts
+++ RecipeBook/src/app/auth/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,13 +11,38 @@ export class AuthComponent implements OnInit {
 
   isLoginMode = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm){
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if(this.isLoginMode){
+      //...
+    }else{
+      //Sign Up mode
+      this.authService.signup(email, password).subscribe(
+      {
+        next:(response) => {
+        console.log(response);
+      },
+      error:(error) => {
+        console.log(error);
+      }
+    }
+    );
+    }
+    form.reset();
   }
 
 }
