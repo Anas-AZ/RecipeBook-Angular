7912003a3e143ed392d2a69d6b7e8a867ec7c16e
Anas <anas9991az@gmail.com>
2023-05-31 11:58:07 +0530

    creating and storing user data

--- RecipeBook/src/app/services/auth.service.ts
+++ RecipeBook/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { Subject, catchError, tap, throwError } from 'rxjs';
+import { User } from '../shared/user.model';
 
 
 
@@ -20,6 +21,8 @@ export interface AuthResponseData{
 
 export class AuthService {
 
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {
    }
 
@@ -29,7 +32,10 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    }).pipe(catchError(this.handleError));
+    }).pipe(catchError(this.handleError), tap(resData =>{
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+      })
+      );
    }
 
    login(email: string, password: string){
@@ -37,7 +43,22 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    }).pipe(catchError(this.handleError));
+    }).pipe(catchError(this.handleError),  tap(resData =>{
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+      })
+      );
+   }
+
+
+   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
+
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+
+      const user = new User(
+        email, userId, token, expirationDate
+      )
+
+      this.user.next(user);
    }
 
    private handleError(errorRes: HttpErrorResponse){
