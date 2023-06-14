718b1df0bd1f37d6412eef2d9a7486e41121efe3
Anas <anas9991az@gmail.com>
2023-06-14 12:19:45 +0530

    added authguardfn

--- RecipeBook/src/app/services/auth-guard.service.ts
+++ RecipeBook/src/app/services/auth-guard.service.ts
@@ -1,23 +1,36 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, map, take } from 'rxjs';
 import { AuthService } from './auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuardService implements CanActivate{
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuardService implements CanActivate{
 
-  constructor(private authService: AuthService, private router: Router) { }
+//   constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot,
-     state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree>| Promise<boolean | UrlTree> {
-      return this.authService.user.pipe(take(1), map(user => {
-        const isAuth = user ? true : false;
-      if(isAuth) {
+//   canActivate(route: ActivatedRouteSnapshot,
+//      state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree>| Promise<boolean | UrlTree> {
+//       return this.authService.user.pipe(take(1), map(user => {
+//         const isAuth = user ? true : false;
+//       if(isAuth) {
+//         return true;
+//       }
+//        return this.router.createUrlTree(['/auth']);
+//       }))
+//   }
+// }
+
+export const AuthGuardService : CanActivateFn =
+(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : Observable<boolean|UrlTree> => {
+    const router = inject(Router);
+    const authService = inject(AuthService);
+  return authService.user.pipe(map( user => {
+    const isAuth = !!user;
+    if(isAuth) {
         return true;
-      }
-       return this.router.createUrlTree(['/auth']);
-      }))
-  }
-}
+    }
+    return router.createUrlTree(['/auth']);
+  }));
+};
