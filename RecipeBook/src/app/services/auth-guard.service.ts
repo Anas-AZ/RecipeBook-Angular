24f9018c8e8359ba14e1b72b3fce38b4192c0a93
Anas <anas9991az@gmail.com>
2023-06-14 12:10:55 +0530

    modified authguard to include urltree

--- RecipeBook/src/app/services/auth-guard.service.ts
+++ RecipeBook/src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, map, take } from 'rxjs';
 import { AuthService } from './auth.service';
 
@@ -11,10 +11,13 @@ export class AuthGuardService implements CanActivate{
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot,
-     state: RouterStateSnapshot): boolean | Observable<boolean>| Promise<boolean> {
+     state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree>| Promise<boolean | UrlTree> {
       return this.authService.user.pipe(take(1), map(user => {
-      if(user) return true;
-       else this.router.navigate(['/auth']);
+        const isAuth = user ? true : false;
+      if(isAuth) {
+        return true;
+      }
+       return this.router.createUrlTree(['/auth']);
       }))
   }
 }
